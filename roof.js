11c1c2d8a3034b02dedbe244ce19df2ce251e1be
aravindhrajan13 <72081624+aravindhrajan13@users.noreply.github.com>
2020-10-20 19:56:36 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,40 @@
+class Roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.width = width;
+      this.height = height;
+      this.x=x;
+      this.y=y;
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      strokeWeight(4);
+      fill(255,0,255);
+      rect(0,0, this.width, this.height);
+      pop();
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
